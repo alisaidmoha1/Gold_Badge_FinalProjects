962e485cf1d2fbb27fc764a262b398998f059cbc
Said <alisaidmoha1@gmail.com>
2021-08-23 11:38:17 -0400

    Started Project Insurance

---
+++ KomodoInsurance/Insurance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoInsurance
+{
+    public enum DoorNames { A1, A2, A3, A4, A5, B1, B2, B3, B4, B5}
+    public class Insurance
+    {
+        public int  BadgeID { get; set; }
+        public DoorNames AccessDooor { get; set; }
+
+        public Insurance() { }
+
+        public Insurance(int badgeID, DoorNames accessDoor)
+        {
+            BadgeID = badgeID;
+            AccessDooor = accessDoor;
+        }
+    }
+}
---
+++ KomodoInsurance/Insurance_Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoInsurance
+{
+    public class Insurance_Repository
+    {
+        Dictionary<int, DoorNames> _listOfBadges = new Dictionary<int, DoorNames>();
+
+        public void CreateNewBadges(int badgeID, DoorNames doorNames)
+        {
+            _listOfBadges.Add(badgeID, doorNames);
+        }
+
+        public Dictionary<int, DoorNames> GetAllBadges()
+        {
+            return _listOfBadges;
+        }
+
+        public bool UpdateBadges(int badgeID, DoorNames newDoorNames)
+        {
+            Insurance oldAccess = GetAccessByBadge(badgeID);
+
+            if (oldAccess == null)
+            {
+                oldAccess.AccessDooor = newDoorNames;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        //Helper
+
+        public Insurance GetAccessByBadge(int badgeID)
+        {
+            foreach (KeyValuePair<int, DoorNames> accessKeyValue in _listOfBadges)
+            {
+               badgeID = accessKeyValue.Key;
+            }
+
+            return null;
+        }
+
+    }
+
+}
